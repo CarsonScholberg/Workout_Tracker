438038020b013a9317c1efbad5fbaf6a4cc0163b
Carson Scholberg <cscholbe@gustavus.edu>
2021-02-24 17:02:31 -0600

    get, post, and put rounds for workouts.js etc

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const logger = require("morgan")
 const path = require("path")
+const mongoose = require("mongoose")
+const db = require("./models");
 
 const PORT = process.env.PORT || 3000;
 
@@ -11,6 +13,11 @@ app.use(logger("dev"));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static("public"))
+
+mongoose.connect("mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
 
 app.get("/", (req,res) => {
     res.sendFile(path.join(__dirname, "./public/index.html"))
@@ -24,7 +31,45 @@ app.get("/stats", (req,res) => {
     res.sendFile(path.join(__dirname, "./public/stats.html"))
 });
 
+app.get("/api/workouts", (req, res) => {
+    db.Workout.find({})
+        .then(data => {
+            res.json(data)
+        })
+})
 
+let totalDuration;
+
+app.post("/api/workouts", (req, res) => {
+    totalDuration=0;
+    db.Workout.create({})
+        .then(created => {
+            res.json(created)
+        })
+})
+
+app.put("/api/workouts/:id", (req, res) => {
+    let id = req.params.id;
+    let data = req.body;
+
+    totalDuration += data.duration;
+
+    db.Workout.updateOne({_id: id},
+        {
+            $push: {exercises: data},
+            totalDuration: totalDuration
+        }
+    ).then(updated => {
+        res.send(updated)
+    })
+})
+
+app.get("/api/workouts/range", (req, res) => {
+    db.Workout.find({})
+        .then(data => {
+            res.json(data)
+        })
+})
 app.listen(PORT, ()=> {
     console.log("ay")
 })
